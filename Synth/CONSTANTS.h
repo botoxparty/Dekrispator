e17bfc632d8b259d1c2b28a03993e75351a7804f
Tomas <tomas.scherrer@gmail.com>
2014-08-12 00:26:45 +0900

    Reduced buffer to 8*4=32 samples

--- Synth/CONSTANTS.h
+++ Synth/CONSTANTS.h
@@ -15,7 +15,7 @@
 #define _2PI                    6.283185307f
 #define _PI                    	3.14159265f
 
-#define BUFF_LEN_DIV4           400 // number of samples in the audiobuffer for each channel <==> XX ms latency at 48kHz
+#define BUFF_LEN_DIV4           8 // number of samples in the audiobuffer for each channel <==> XX ms latency at 48kHz
 #define BUFF_LEN_DIV2           (2*BUFF_LEN_DIV4)
 #define BUFF_LEN                (4*BUFF_LEN_DIV4)  // Audio buffer length : count in 16bits half-words
 #define VOL                     70 // initial output DAC volume
